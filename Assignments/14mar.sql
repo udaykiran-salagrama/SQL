f83e9edd035d12ae4af954620c14fad988dc6232
udaykiran-salagrama <udaykiran.s@hotmail.com>
2025-03-14 10:12:36 +0530

    Update 14mar.sql

--- Assignments/14mar.sql
+++ Assignments/14mar.sql
@@ -5,6 +5,11 @@ GROUP BY department_id;
 
 
 SELECT department_id, AVG(salary) AS avg_salary
-FROM employees
+FROM HR.EMPLOYEES
 GROUP BY department_id; 
 --query to find the average salary in each department
+
+SELECT department_id, MAX(salary)  Max_salary, MIN(salary) Min_salary
+FROM HR.EMPLOYEES
+GROUP BY department_id;
+--Query to find the maximum and minimum salary in each department
